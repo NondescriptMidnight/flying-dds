fad9f0a4de8d501c120e78f0a9daff91a9735c69
Stacey-Eva Fabrè <59258519+NondescriptMidnight@users.noreply.github.com>
2019-12-26 23:28:59 +0200

    Controllers, prefabs and scenes

---
+++ 2D Platformer Controller/Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonManager : MonoBehaviour
+{ 
+
+    // Use this for initialization
+    public void Repeat()
+    {
+        ScoreTrackering.scoreNum = 0;
+        Application.LoadLevel(0);
+    }
+    public void Continue()
+    {
+        Application.OpenURL("http://www.dddragon.cn/endgame.php");
+    }
+}
---
+++ 2D Platformer Controller/Scripts/ScoreDisp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDisp : MonoBehaviour {
+
+    public float screLft;
+    public float screDwn;
+
+    void OnGUI()
+    {
+        GUIStyle scorer = new GUIStyle(GUI.skin.GetStyle("label"));
+        scorer.fontSize = 50;
+        scorer.normal.textColor = Color.red;
+
+        GUI.Label(new Rect(screLft, screDwn, 500, 100), "SCORE:  " + ScoreTrackering.scoreNum, scorer);
+
+    }
+}
+
+
